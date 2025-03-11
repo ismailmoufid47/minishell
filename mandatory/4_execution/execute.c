e617e6dc003b7841a988a8aaec84ae5b3df7ce8e
ismailmoufid47 <ismailmoufid47@gmail.com>
2025-03-11 22:51:07 +0000

    cmd execution (maybe)

--- mandatory/4_execution/execute.c
+++ mandatory/4_execution/execute.c
@@ -1,12 +1,11 @@
 #include "../include/shell.h"
 
-void	redirect(t_list *cmd)
+void	redirect(t_list *redirections)
 {
 	t_list	*current;
 	char	*file;
-	char	*input;
 
-	current = cmd->redirections;
+	current = redirections;
 	while (current)
 	{
 		file = current->next->value;
@@ -22,15 +21,67 @@ void	redirect(t_list *cmd)
 	}
 }
 
+void	pipe_wrapper(int *pipe_fd)
+{
+	if (pipe(pipe_fd) == -1)
+		error(ft_strdup("pipe"));
+}
+
+char	**envp_to_char(t_envp *envp)
+{
+	int		count;
+	t_envp	*current;
+	char	**result;
+	char	*tmp;
+
+	current = envp;
+	while (current)
+	{
+		count++;
+		current->next;
+	}
+	result = malloc((count + 1) * sizeof(char *));
+	count = 0;
+	while (envp)
+	{
+		tmp = ft_strjoin(envp->name, "=");
+		result[count] = ft_strjoin(tmp, envp->value);
+		free(tmp);
+		count++;
+		envp = envp->next;
+	}
+	return (result);
+}
+
+void	execute_cmd(t_list *cmd, t_envp *envp)
+{
+	char	**envp_char;
+	char	**args;
+
+	if (cmd->is_redirected)
+		redirect(cmd->redirections);
+	envp_char = envp_to_char(envp);
+	args = ft_split(cmd->value);
+
+}
+
 void	execute(t_list *list, t_envp *envp)
 {
 	t_list	*current;
+	int		pipe_fd[2];
+	int		pid;
 
 	current = list;
 	while (current)
 	{
-		if (current->type == CMD && current->is_redirected)
-			redirect(current);
+		if (current->next && current->next->type == PIPE)
+		{
+			pipe_wrapper(pipe_fd);
+			ft_dup2(pipe_fd[1], STDOUT_FILENO);
+			pid = fork();
+			if (pid == 0)
+				execute_cmd(current, envp);
+		}
 		current = current->next;
 	}
 }
