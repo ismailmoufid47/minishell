86f1778efa794ceb43f6e17f0a5e053df4ee3adf
ismailmoufid47 <ismailmoufid47@gmail.com>
2025-05-25 20:59:34 +0100

    testing wildcard handling

--- mandatory/include/shell.h
+++ mandatory/include/shell.h
@@ -4,21 +4,21 @@
 
 
 # include "../../libft/include/libft.h"
-# include "readline/readline.h" // readline, rl_on_new_line, rl_replace_line, rl_redisplay
-# include "readline/history.h"  // rl_clear_history, add_history
+# include <stdio.h>    		    		// printf
+# include "readline/readline.h" 		// readline, rl_on_new_line, rl_replace_line, rl_redisplay
+# include "readline/history.h"  		// rl_clear_history, add_history
 # include "ncurses/ncurses.h"
-# include <stdlib.h>         // malloc, free, exit, getenv
-# include <unistd.h>         // write, access, open, read, close, fork, getcwd, chdir, execve, dup, dup2, pipe, isatty, ttyname
-# include <stdio.h>          // printf
-# include <string.h>         // strerror
-# include <signal.h>         // signal, sigaction, sigemptyset, sigaddset, kill
-# include <dirent.h>         // opendir, readdir, closedir
-# include <sys/types.h>      // wait, waitpid, wait3, wait4, stat, lstat, fstat
-# include <fcntl.h>          // open, unlink
-# include <sys/ioctl.h>      // ioctl
-# include <termios.h>        // tcsetattr, tcgetattr
-# include <term.h>           // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (alternative to <curses.h>)
-# include <errno.h>          // perror
+# include <stdlib.h>        			// malloc, free, exit, getenv
+# include <unistd.h>         			// write, access, open, read, close, fork, getcwd, chdir, execve, dup, dup2, pipe, isatty, ttyname
+# include <string.h>         			// strerror
+# include <signal.h>         			// signal, sigaction, sigemptyset, sigaddset, kill
+# include <dirent.h>         			// opendir, readdir, closedir
+# include <sys/types.h>      			// wait, waitpid, wait3, wait4, stat, lstat, fstat
+# include <fcntl.h>          			// open, unlink
+# include <sys/ioctl.h>      			// ioctl
+# include <termios.h>        			// tcsetattr, tcgetattr
+# include <term.h>           			// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (alternative to <curses.h>)
+# include <errno.h>          			// perror
 # include <sys/wait.h>
 
 extern char	**environ;
