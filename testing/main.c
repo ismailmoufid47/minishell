9d6be5260e2110e3d98adb224218573fde25be1c
jbelkerf <jbelkerf@gmail.com>
2025-05-25 13:43:09 +0100

    cocking

--- testing/main.c
+++ testing/main.c
@@ -6,7 +6,7 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 12:50:37 by jbelkerf          #+#    #+#             */
-/*   Updated: 2025/05/25 13:18:42 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2025/05/25 13:42:43 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,50 @@
 #include "../mandatory/include/shell.h"
 #include <dirent.h>
 
+int	match_front(char *file, char *front)
+{
+	int i = 0;
+	while (file[i] && front[i])
+	{
+		if (file[i] != front[i])
+			break ;
+		i++;
+	}
+	if (front[i] == 0)
+		return 1;
+	return 0;
+}
+
+char *rev_str(char *str)
+{
+	char *rev = strdup(str);
+	int start = 0;
+	int len = strlen(str) - 1;
+	char tmp;
+	while (start < len)
+	{
+		tmp = rev[start];
+		rev[start] = rev[len];
+		rev[len] = tmp;
+		start++;
+		len--;
+	}
+	return rev;
+}
+
+int match_end(char *file , char *end)
+{
+	file = rev_str(file);
+	end = rev_str(end);
+	return (match_front(file, end));
+}
+
+void	print_files(char **files)
+{
+	int i = 0;
+	while (files[i])
+		printf("--> %s\n", files[i++]);
+}
 
 int	main()
 {
@@ -29,7 +73,9 @@ int	main()
 	while (entry != NULL)
 	{
 		entry = readdir(dir);
-		if ( entry && printf("---> %s\n", entry->d_name))
-		;
+		if (entry)
+			files[i++] = entry->d_name;
 	}
+	files[i] = NULL;
+	print_files(files);
 }
