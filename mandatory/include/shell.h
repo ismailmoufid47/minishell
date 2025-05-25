65cbd4a8efa25a3460101c755e3a23fc010ae933
ismailmoufid47 <ismailmoufid47@gmail.com>
2025-05-25 18:00:40 +0100

    updated header files

--- mandatory/include/shell.h
+++ mandatory/include/shell.h
@@ -4,11 +4,12 @@
 
 
 # include "../../libft/include/libft.h"
+# include "readline/readline.h" // readline, rl_on_new_line, rl_replace_line, rl_redisplay
+# include "readline/history.h"  // rl_clear_history, add_history
+# include "ncurses/ncurses.h"
 # include <stdlib.h>         // malloc, free, exit, getenv
 # include <unistd.h>         // write, access, open, read, close, fork, getcwd, chdir, execve, dup, dup2, pipe, isatty, ttyname
 # include <stdio.h>          // printf
-# include <readline/readline.h> // readline, rl_on_new_line, rl_replace_line, rl_redisplay
-# include <readline/history.h>  // rl_clear_history, add_history
 # include <string.h>         // strerror
 # include <signal.h>         // signal, sigaction, sigemptyset, sigaddset, kill
 # include <dirent.h>         // opendir, readdir, closedir
@@ -16,7 +17,6 @@
 # include <fcntl.h>          // open, unlink
 # include <sys/ioctl.h>      // ioctl
 # include <termios.h>        // tcsetattr, tcgetattr
-# include <curses.h>         // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (alternative: <term.h>)
 # include <term.h>           // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (alternative to <curses.h>)
 # include <errno.h>          // perror
 # include <sys/wait.h>
